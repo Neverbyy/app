3344756b58ab7182547a11d2e73bb3f7cf7867b3
Artem <mister_paint03@mail.ru>
2026-01-26 14:40:19 +0300

    fix

--- config.shared.ts
+++ config.shared.ts
@@ -1,4 +1,4 @@
-export const DEFAULT_API_URL = "https://stage.sofi-assistant.com/api";
+export const DEFAULT_API_URL = "https://sofi-assistant.com/api";
 
 export const getApiDomain = (apiUrl: string): string => {
   return apiUrl.replace(/\/api\/?$/, '');
